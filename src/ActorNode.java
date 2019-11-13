296cbbf7073f42a363c49699425018adbca23347
Ted Clifford <edwardfclifford@gmail.com>
2019-11-13 12:52:13 -0500

    Added equals and hashcode methods to nodes

--- src/ActorNode.java
+++ src/ActorNode.java
@@ -14,5 +14,16 @@ public class ActorNode implements Node {
     public Collection<MovieNode> getNeighbors () {
         return _neighbors;
     }
+
+    @Override
+    public boolean equals (Object o) {
+        final ActorNode other = (ActorNode) o;
+        return _name == other._name && _neighbors.equals(other._neighbors);
+    }
+
+    @Override int hashCode () {
+        final String hashString = "" + _name.hashCode() + _neighbors.hashCode();
+        return hashString.hashCode();
+    }
 }
 
--- src/MovieNode.java
+++ src/MovieNode.java
@@ -15,4 +15,14 @@ public class MovieNode implements Node {
         return _neighbors;
     }
 
+    @Override
+    public boolean equals (Object o) {
+        final ActorNode other = (ActorNode) o;
+        return _name == other._name && _neighbors.equals(other._neighbors);
+    }
+
+    @Override int hashCode () {
+        final String hashString = "" + _name.hashCode() + _neighbors.hashCode();
+        return hashString.hashCode();
+    }
 }
